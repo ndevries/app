b213d85e3279c6982038145e39e1cf8e410ee08f
Nicholas DeVries <nicholas@thirtyfive.io>
2013-12-04 09:22:31 -0500

    various updates

--- js/app.js
+++ js/app.js
@@ -1,31 +1,19 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(document).foundation();
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+document.addEventListener('deviceready', this.onDeviceReady, false);
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onDeviceReady() {
+	console.log("deviceready");
+}
+
+function togglePlayPause() {
+	var audio = document.getElementById('audio');
+	var controls = document.getElementById('controls');
+	if (audio.paused || audio.ended) {
+		controls.innerHTML = "<i class='fa fa-pause'></i>";
+		audio.play();
+	} else {
+		controls.innerHTML = "<i class='fa fa-play'></i>";
+		audio.pause();
+	}
+}
