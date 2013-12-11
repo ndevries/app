51c53e03a71f37db98f8a71db6557ab987b5993c
Nicholas DeVries <nicholas@thirtyfive.io>
2013-12-11 08:24:12 -0500

    updated animations and other stuff

--- api/process.php
+++ api/process.php
@@ -2,77 +2,92 @@
 
 if ($_GET["type"] == "audios") {
 
-	// Logic for audio
+    // Logic for audio
 
-	if (isset($_GET["id"])) {
+    if (isset($_GET["id"])) {
 
-		$data = array(
-			"name" => "Audio " . $_GET["id"],
-			"author" => "William Shakespeare",
-			"url" => "http://birdnote.s3.amazonaws.com/Birdnote/2011/07-Jul-2011/110709-Marbled-Murrelet.mp3",
-			"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-		);
+        $status = array(
+            "error"   => false,
+            "message" => ""
+        );
 
-	} else {
+        $data = array(
+            "name"        => "Audio " . $_GET["id"],
+            "author"      => "William Shakespeare",
+            "url"         => "http://birdnote.s3.amazonaws.com/Birdnote/2011/07-Jul-2011/110709-Marbled-Murrelet.mp3",
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        );
 
-		for ($i = 0; $i < 7; $i++) { 
+    } else {
 
-			$data[] = array(
-				"name" => "Audio " . ($i + 1),
-				"id" => "" . $i + 1 . "",
-				"author" => "William Shakespeare"
-			);
+        $status = array(
+            "error"   => false,
+            "message" => ""
+        );
 
-		}
+        for ($i = 0; $i < 7; $i++) {
 
-	}
+            $data[] = array(
+                "name"   => "Audio " . ($i + 1),
+                "id"     => "" . $i + 1 . "",
+                "author" => "William Shakespeare"
+            );
+
+        }
+
+    }
 
 } elseif ($_GET["type"] == "videos") {
 
-	// Logic for video
+    // Logic for video
 
-	if (isset($_GET["id"])) {
+    if (isset($_GET["id"])) {
 
-		$data = array(
-			"name" => "Video " . $_GET["id"],
-			"author" => "William Shakespeare",
-			"url" => "http://vjs.zencdn.net/v/oceans.mp4",
-			"description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-		);
+        $data = array(
+            "name"        => "Video " . $_GET["id"],
+            "author"      => "William Shakespeare",
+            "url"         => "http://vjs.zencdn.net/v/oceans.mp4",
+            "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        );
 
-	} else {
+    } else {
 
-		for ($i = 0; $i < 7; $i++) { 
+        for ($i = 0; $i < 7; $i++) {
 
-			$data[] = array(
-				"name" => "Video " . ($i + 1),
-				"id" => "" . $i + 1 . "",
-				"author" => "William Shakespeare"
-			);
+            $data[] = array(
+                "name"   => "Video " . ($i + 1),
+                "id"     => "" . $i + 1 . "",
+                "author" => "William Shakespeare"
+            );
 
-		}
+        }
 
-	}
+    }
 
 } elseif ($_GET["type"] == "users") {
 
-	// Logic for user
+    // Logic for user
 
-	$data = array(
-		"name" => "Nicholas",
-		"password" => "password"
-	);
+    $data = array(
+        "id"       => "12345",
+        "name"     => "Nicholas",
+        "password" => "password"
+    );
 
 } else {
 
-	// Error message
+    // Error message
 
-	$data = array(
-		"error" => "Invalid type"
-	);
+    $data = array(
+        "error" => "Invalid type"
+    );
 
 }
 
+// Create json
+$json["status"][] = $status;
+$json["data"][] = $data;
+
 // Return json
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($json);
--- js/controllers.js
+++ js/controllers.js
@@ -1,32 +1,33 @@
 var myControllers = angular.module('myControllers', []);
 
-myControllers.controller('AudioListController', ['$scope', '$http', function($scope, $http) {
+myControllers.controller('AudioListController', function($scope, $http) {
 	$http.get('api/process.php?type=audios').success(function(data) {
 		$scope.audios = data;
 	});
-}]);
+});
 
-myControllers.controller('AudioSingleController', ['$scope', '$routeParams', '$http', '$sce', function($scope, $routeParams, $http, $sce) {
+myControllers.controller('AudioSingleController', function($scope, $routeParams, $http, $sce) {
 	$http.get('api/process.php?type=audios&id=' + $routeParams.audioId).success(function(data) {
 		$scope.audio = data;
 		$scope.audio.url = $sce.trustAsResourceUrl($scope.audio.url);
 	});
-}]);
+});
 
-myControllers.controller('VideoListController', ['$scope', '$http', function($scope, $http) {
+myControllers.controller('VideoListController', function($scope, $http) {
 	$http.get('api/process.php?type=videos').success(function(data) {
 		$scope.videos = data;
 	});
-}]);
+});
 
-myControllers.controller('VideoSingleController', ['$scope', '$routeParams', '$http', '$sce', function($scope, $routeParams, $http, $sce) {
+myControllers.controller('VideoSingleController', function($scope, $routeParams, $http, $sce) {
 	$http.get('api/process.php?type=videos&id=' + $routeParams.videoId).success(function(data) {
 		$scope.video = data;
 		$scope.video.url = $sce.trustAsResourceUrl($scope.video.url);
+		$('video').play();
 	});
-}]);
+});
 
-myControllers.controller('LoginController', ['$scope', '$http', function($scope, $http) {
+myControllers.controller('LoginController', function($scope, $http) {
 	$scope.user = {};
 	$http.get('api/process.php?type=users').success(function(data) {
 		$scope.user = data;
@@ -39,4 +40,4 @@ myControllers.controller('LoginController', ['$scope', '$http', function($scope,
 			alert('Invalid!');
 		}
 	};
-}]);
+});
