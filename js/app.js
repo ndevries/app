abb77ac2239da019227357750034bb8cc11bd1b4
Nicholas DeVries <nicholas@thirtyfive.io>
2013-12-16 11:13:45 -0500

    animation and header changes

--- js/app.js
+++ js/app.js
@@ -7,18 +7,17 @@ var app = angular.module('app', [
 	'myControllers'
 ]);
 
-// Routing
 app.config(function($routeProvider) {
-	$routeProvider.when('/', {templateUrl: 'views/Landing.html'});
-	$routeProvider.when('/menu', {templateUrl: 'views/Menu.html'});
+	$routeProvider.when('/', {templateUrl: 'views/Landing.html', controller: 'LandingController'});
+	$routeProvider.when('/menu', {templateUrl: 'views/Menu.html', controller: 'MenuController'});
 	$routeProvider.when('/login', {templateUrl: 'views/Login.html', controller: 'LoginController'});
 	$routeProvider.when('/logout', {template: ' ', controller: 'LogoutController'});
-	$routeProvider.when('/public', {templateUrl: 'views/Public.html'});
-	$routeProvider.when('/messages', {templateUrl: 'views/MessageList.html'});
-	$routeProvider.when('/resources', {templateUrl: 'views/ResourceList.html'});
 	$routeProvider.when('/audios', {templateUrl: 'views/AudioList.html', controller: 'AudioListController'});
 	$routeProvider.when('/audios/:audioId', {templateUrl: 'views/AudioSingle.html', controller: 'AudioSingleController'});
 	$routeProvider.when('/videos', {templateUrl: 'views/VideoList.html', controller: 'VideoListController'});
 	$routeProvider.when('/videos/:videoId', {templateUrl: 'views/VideoSingle.html', controller: 'VideoSingleController'});
+	$routeProvider.when('/messages', {templateUrl: 'views/MessageList.html', controller: 'MessageListController'});
+	$routeProvider.when('/resources', {templateUrl: 'views/ResourceList.html', controller: 'ResourceListController'});
+	$routeProvider.when('/public', {templateUrl: 'views/Public.html', controller: 'PublicController'});
 	$routeProvider.otherwise({redirectTo: '/'});
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,58 +1,170 @@
 var myControllers = angular.module('myControllers', []);
 
-myControllers.controller('MainController', function($scope, $location) {
-    // main actions
+myControllers.controller('MainController', function($scope, $location, $rootScope, $window) {
+
+    // header options
+    $rootScope.showNav  = false;
+    $rootScope.showMenu = false;
+    $rootScope.title    = '';
+
+    // set transition animations based on action
+    $scope.slide = '';
+    $rootScope.go = function(path){
+        $scope.slide = 'slide-left';
+        $location.url(path);
+    }
+    $rootScope.goBack = function(path){
+        $scope.slide = 'slide-right';
+        $location.url(path);
+    }
+    $rootScope.back = function() {
+        $scope.slide = 'slide-right';
+        $window.history.back();
+    }
+
 });
 
-myControllers.controller('AudioListController', function($scope, $http) {
+myControllers.controller('MenuController', function($rootScope) {
+
+    // header options
+    $rootScope.showNav  = false;
+    $rootScope.showMenu = false;
+    $rootScope.title    = '';
+
+});
+
+myControllers.controller('LandingController', function($rootScope) {
+
+    // header options
+    $rootScope.showNav  = false;
+    $rootScope.showMenu = false;
+    $rootScope.title    = '';
+
+});
+
+myControllers.controller('LoginController', function($scope, $http, $rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = false;
+    $rootScope.title    = 'Login';
+
+    // run when valid form is submitted
+    $scope.submit = function(form) {
+
+        // call api
+        $http.get('api/process.php?type=users&user=' + $scope.user.name + '&pass=' + $scope.user.password).success(function(data) {
+            $scope.json   = data;
+            $scope.errors = $scope.json.status;
+
+            // if no errors are returned set localstorage values and redirect
+            if ($scope.errors.error == false) {
+                window.localStorage.id     = $scope.json.data.id;
+                window.localStorage.secret = $scope.json.data.secret;
+                $rootScope.go('/menu');
+            }
+        });
+    };
+
+});
+
+myControllers.controller('LogoutController', function($rootScope) {
+
+    // clear localstorage and redirect
+    window.localStorage.clear();
+    $rootScope.goBack('/');
+
+});
+
+myControllers.controller('AudioListController', function($scope, $http, $rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = true;
+    $rootScope.title    = 'Listen';
+
+    // call api
     $http.get('api/process.php?type=audios').success(function(data) {
         $scope.json   = data;
         $scope.audios = $scope.json.data;
         $scope.errors = $scope.json.status;
     });
+
 });
 
-myControllers.controller('AudioSingleController', function($scope, $routeParams, $http, $sce) {
+myControllers.controller('AudioSingleController', function($scope, $routeParams, $http, $sce, $rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = false;
+    $rootScope.title    = 'Listen';
+
+    // call api
     $http.get('api/process.php?type=audios&id=' + $routeParams.audioId).success(function(data) {
         $scope.json      = data;
         $scope.audio     = $scope.json.data;
         $scope.errors    = $scope.json.status;
         $scope.audio.url = $sce.trustAsResourceUrl($scope.audio.url);
     });
+
 });
 
-myControllers.controller('VideoListController', function($scope, $http) {
+myControllers.controller('VideoListController', function($scope, $http, $rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = true;
+    $rootScope.title    = 'Watch';
+
+    // call api
     $http.get('api/process.php?type=videos').success(function(data) {
         $scope.json   = data;
         $scope.videos = $scope.json.data;
         $scope.errors = $scope.json.status;
     });
+
 });
 
-myControllers.controller('VideoSingleController', function($scope, $routeParams, $http, $sce) {
+myControllers.controller('VideoSingleController', function($scope, $routeParams, $http, $sce, $rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = false;
+    $rootScope.title    = 'Watch';
+
+    // call api
     $http.get('api/process.php?type=videos&id=' + $routeParams.videoId).success(function(data) {
         $scope.json      = data;
         $scope.video     = $scope.json.data;
         $scope.errors    = $scope.json.status;
         $scope.video.url = $sce.trustAsResourceUrl($scope.video.url);
     });
+
 });
 
-myControllers.controller('LoginController', function($scope, $http, $location) {
-    $scope.submit = function(form) {
-        $http.get('api/process.php?type=users&user=' + $scope.user.name + '&pass=' + $scope.user.password).success(function(data) {
-            $scope.json   = data;
-            $scope.errors = $scope.json.status;
-            if ($scope.errors.error == false) {
-                window.localStorage.id     = $scope.json.data.id;
-                window.localStorage.secret = $scope.json.data.secret;
-                $location.path('/audios');
-            }
-        });
-    };
+myControllers.controller('MessageListController', function($rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = true;
+    $rootScope.title    = 'Messages';
+
 });
 
-myControllers.controller('LogoutController', function($location) {
-    window.localStorage.clear();
-    $location.path('/');
+myControllers.controller('ResourceListController', function($rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = true;
+    $rootScope.title    = 'Resources';
+
+});
+
+myControllers.controller('PublicController', function($rootScope) {
+
+    // header options
+    $rootScope.showNav  = true;
+    $rootScope.showMenu = false;
+    $rootScope.title    = 'Public';
+
 });
